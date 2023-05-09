edd292729d648e4eaad237bfb616dd2637dddc33
tqk2811 <tqk2811@gmail.com>
2023-05-09 10:36:11 +0700

    remove targets

--- Build.ps1
+++ Build.ps1
@@ -2,6 +2,12 @@ Remove-Item -Recurse -Force .\Release\** -ErrorAction SilentlyContinue
 nuget pack .\TqkLibrary.FFmpeg.GplShared.nuspec -OutputDirectory .\Release
 nuget pack .\TqkLibrary.FFmpeg.Runtimes.nuspec -OutputDirectory .\Release
 
+$localNuget = $env:localNuget
+if(![string]::IsNullOrWhiteSpace($localNuget))
+{
+    Copy-Item .\Release\*.nupkg -Destination $localNuget -Force
+}
+
 $nugetKey=$env:nugetKey
 if([string]::IsNullOrEmpty($nugetKey))
 {
